96b6cbed94eec242232ecf07cc7d25e3942cbb74
Camilo <46683077+k4dm3l@users.noreply.github.com>
2026-07-19 22:08:20 -0500

    removed commented code

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,4 @@
 import 'dotenv/config';
-import { MongoClient } from 'mongodb';
-
-/**
-MONGO_DB_URI="mongodb+srv://support:qaYKG7nxUykRd8EP@gosafeagency.qgiayte.mongodb.net/intranet-production"
-RABBITMQ_URI="amqp://Ak0JvF62j0TfDVFB:wsdG~ra-QTcxCq~zg0ynmbXGrR_QwNbo@rabbitmq.railway.internal:5672"
-SOAP_FORCE_IPV4=true
-SOAP_HTTP_TIMEOUT_MS=120000
-WSDL_PREFETCH=true
-WSDL_URL="https://expressnet.iix.com/web-services/Auth?WSDL"
-WSDL_DISABLE_CACHE=true
- */
 
 export const config = {
   mongoDBUri: process.env.MONGO_DB_URI ?? '',
